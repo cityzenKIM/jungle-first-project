da320da735766f7dafd983d772d4b9eaee015f06
KKamjangE <ajm980718@gmail.com>
2022-10-25 08:47:28 +0900

    set

--- app.py
+++ app.py
@@ -7,8 +7,20 @@ client = MongoClient('localhost', 27017)
 db = client.dblaundry
 
 @app.route('/')
-def home():
+def listPage():
     return render_template('index.html')
+
+@app.route('/reservepage')
+def reservePage():
+    return render_template('reserve.html')
+
+@app.route('/loginpage')
+def loginPage():
+    return render_template('login.html')
+
+@app.route('/signuppage')
+def signupPage():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
