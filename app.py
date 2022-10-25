2dfa381e458a93bb28b52331cd54c082afff5af7
hoondal69 <davidtwins6970@gmail.com>
2022-10-25 20:37:55 +0900

    초안_수정1

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 dblaundry = client.dblaundry
 
-# days = ['월','화','수','목','금','토','일']
-# times = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-# for day in days:
-#     for time in times:
-#         dblaundry.dblaundry.insert_one({'time':'time','요일':'day','이용자':'','예약자id':''})
 
 @app.route('/')
 def listPage():
-    # laundrydata = list(dblaundry.week.find({},{'_id':0}))
-    # laundrydata_07 = list(dblaundry.week.find({'요일':'월'},{'_id':0}))
-    laundrydata_mon = list(dblaundry.week.find({},{'_id':0}))
+    laundrydata_mon = list(dblaundry.week.find({'date':'월'},{'_id':0}))
     laundrydata_tue = list(dblaundry.week.find({'date':'화'},{'_id':0}))
     laundrydata_wed = list(dblaundry.week.find({'date':'수'},{'_id':0}))
     laundrydata_thu = list(dblaundry.week.find({'date':'목'},{'_id':0}))
@@ -32,7 +25,7 @@ def listPage():
     laundrydata_sum.append(laundrydata_fri)
     laundrydata_sum.append(laundrydata_sat)
     laundrydata_sum.append(laundrydata_sun)
-    return render_template('index.html', laundryDatas_sum = laundrydata_mon)
+    return render_template('index.html', laundryDatas_sum = laundrydata_sum)
 
 @app.route('/reservepage')
 def reservePage():
