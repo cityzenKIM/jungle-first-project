57df60af226c5c03ed1100dca453ef8c3b01c32a
hoondal69 <davidtwins6970@gmail.com>
2022-10-25 20:33:13 +0900

    시간표 초안

--- app.py
+++ app.py
@@ -4,11 +4,35 @@ from flask import Flask, render_template, jsonify, Request
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.dblaundry
+dblaundry = client.dblaundry
+
+# days = ['월','화','수','목','금','토','일']
+# times = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+# for day in days:
+#     for time in times:
+#         dblaundry.dblaundry.insert_one({'time':'time','요일':'day','이용자':'','예약자id':''})
 
 @app.route('/')
 def listPage():
-    return render_template('index.html')
+    # laundrydata = list(dblaundry.week.find({},{'_id':0}))
+    # laundrydata_07 = list(dblaundry.week.find({'요일':'월'},{'_id':0}))
+    laundrydata_mon = list(dblaundry.week.find({},{'_id':0}))
+    laundrydata_tue = list(dblaundry.week.find({'date':'화'},{'_id':0}))
+    laundrydata_wed = list(dblaundry.week.find({'date':'수'},{'_id':0}))
+    laundrydata_thu = list(dblaundry.week.find({'date':'목'},{'_id':0}))
+    laundrydata_fri = list(dblaundry.week.find({'date':'금'},{'_id':0}))
+    laundrydata_sat = list(dblaundry.week.find({'date':'토'},{'_id':0}))
+    laundrydata_sun = list(dblaundry.week.find({'date':'일'},{'_id':0}))
+    
+    laundrydata_sum = []
+    laundrydata_sum.append(laundrydata_mon)
+    laundrydata_sum.append(laundrydata_tue)
+    laundrydata_sum.append(laundrydata_wed)
+    laundrydata_sum.append(laundrydata_thu)
+    laundrydata_sum.append(laundrydata_fri)
+    laundrydata_sum.append(laundrydata_sat)
+    laundrydata_sum.append(laundrydata_sun)
+    return render_template('index.html', laundryDatas_sum = laundrydata_mon)
 
 @app.route('/reservepage')
 def reservePage():
