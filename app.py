7a0d772a7a17ca22ba1f9a9fe5fd270aab6e6f76
김태준 <kimtaejun@gimtaejun-ui-MacBookAir.local>
2022-10-26 19:20:06 +0900

    예약화면 UI 구현

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, jsonify, request, redirect
 app = Flask(__name__)
 
-import certifi
-ca=certifi.where()
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://tajunkim:wns41224--@cluster0.bxexa3c.mongodb.net/test', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://tajunkim:wns41224--@cluster0.bxexa3c.mongodb.net/test')
 dblaundry = client.dblaundry
 
 import datetime
